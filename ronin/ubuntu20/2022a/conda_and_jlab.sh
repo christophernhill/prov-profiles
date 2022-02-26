85661fc9399fc9d586bf6e602fb8847c2d6e3269
Chris Hill <cnh@mit.edu>
2022-02-25 22:38:22 -0500

    Update conda_and_jlab.sh

--- ronin/ubuntu20/2022a/conda_and_jlab.sh
+++ ronin/ubuntu20/2022a/conda_and_jlab.sh
@@ -1,3 +1,4 @@
+\rm -fr Miniconda3-py39_4.10.3-Linux-x86_64.sh miniconda3 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-x86_64.sh
 chmod +x Miniconda3-py39_4.10.3-Linux-x86_64.sh
 ./Miniconda3-py39_4.10.3-Linux-x86_64.sh -b -p miniconda3 
