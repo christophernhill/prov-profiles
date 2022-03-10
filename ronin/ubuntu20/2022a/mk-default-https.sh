a232d297aa38c67c421429ee885ad014f914e2db
Chris Hill <cnh@mit.edu>
2022-03-09 22:47:23 -0500

    Update mk-default-https.sh

--- ronin/ubuntu20/2022a/mk-default-https.sh
+++ ronin/ubuntu20/2022a/mk-default-https.sh
@@ -28,9 +28,23 @@ server {
     ssl_certificate /etc/nginx/ssl/nginx.crt;
     ssl_certificate_key /etc/nginx/ssl/nginx.key;
 
-    location / {
-        deny all;
-    }
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files $uri $uri/ =404;
+        }
+
+        # pass PHP scripts to FastCGI server
+        #
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+        #
+        #       # With php-fpm (or other unix sockets):
+                fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
+        #       # With php-cgi (or other tcp sockets):
+        #       fastcgi_pass 127.0.0.1:9000;
+        }
+
 }
 EOFA
 
