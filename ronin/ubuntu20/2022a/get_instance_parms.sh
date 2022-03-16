a59e1960c141e428f12f52065d87ffbbaf7a528c
Chris Hill <cnh@mit.edu>
2022-03-16 14:02:20 -0400

    add parts and cache for rate limit

--- ronin/ubuntu20/2022a/get_instance_parms.sh
+++ ronin/ubuntu20/2022a/get_instance_parms.sh
@@ -2,7 +2,14 @@
 #
 # Get instance parameters
 #
+reg=`curl http://169.254.169.254/latest/meta-data/placement/region`
 az=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
 iid=`curl http://169.254.169.254/latest/meta-data//instance-id`
 
 iuri=${iid}.${az}.researchcomputing.cloud
+
+echo > vm_settings.src
+echo 'INST_ID='${iid}    >> vm_settings.src
+echo 'INST_AZ='${az}     >> vm_settings.src
+echo 'INST_REG='${reg}   >> vm_settings.src
+echo 'INST_URI='${iuri}  >> vm_settings.src
