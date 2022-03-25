48fe13f93b93bd4cf470ef24a3ff44bfa046e268
Chris Hill <cnh@mit.edu>
2022-03-25 15:17:24 -0400

    Update setup_web_pages.sh

--- ronin/ubuntu20/2022a/setup_web_pages.sh
+++ ronin/ubuntu20/2022a/setup_web_pages.sh
@@ -4,5 +4,7 @@
 cp  /home/ubuntu/var/www/html/* /var/www/html
 cat /home/ubuntu/var/www/html/index2.php | sed s/XXXXREPLACE_WITH_MACHINE_URI_HEREXXXX/${INST_URI}/ > /var/www/html/index2.php
 
+( cd var/www ; tar -cvf - php/ ) | ( cd /var/www ; tar -xvf - )
+
 usermod -a -G sudo www-data
 systemctl restart nginx
