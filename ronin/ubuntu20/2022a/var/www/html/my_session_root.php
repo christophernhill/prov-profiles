70cba6f07679980fcc1923806e6a13602116e5cd
Chris Hill <cnh@mit.edu>
2022-03-25 13:25:43 -0400

    Update my_session_root.php

--- ronin/ubuntu20/2022a/var/www/html/my_session_root.php
+++ ronin/ubuntu20/2022a/var/www/html/my_session_root.php
@@ -4,7 +4,7 @@ session_start();
 ?>
 
 <?php
-phpinfo();
+// phpinfo();
 ?>
 
 <?php
@@ -12,7 +12,11 @@ if ( !isset($_SESSION['MY_PHP_AUTH_USER']) ) {
 	echo 'Not logged in';
 } else {
 	echo 'Logged in as ' . $_SESSION['MY_PHP_AUTH_USER'];
-  $_SESSION['RAND_STR_24']=bin2hex(random_bytes(24));
-  echo 'Jupyter ' . 'http://localhost/jupyter/' . $_SESSION['RAND_STR_24']=bin2hex(random_bytes(24));
+	if ( !array_key_exists('RAND_STR_24',$_SESSION) ) {
+          $_SESSION['RAND_STR_24']=bin2hex(random_bytes(24));
+	}
+	echo '</br>';
+        echo 'Jupyter ' . '<A href=/start_or_attach_to_jupyter_session.php>' . 'http://localhost/jupyter/' . $_SESSION['RAND_STR_24'] . '</A>';
 }
 ?>
+
