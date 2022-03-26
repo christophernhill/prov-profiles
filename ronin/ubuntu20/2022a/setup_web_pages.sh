64d0865311f6c3ae4669d8e28139d70cc7e35f22
Chris Hill <cnh@mit.edu>
2022-03-25 21:52:07 -0400

    Update setup_web_pages.sh

--- ronin/ubuntu20/2022a/setup_web_pages.sh
+++ ronin/ubuntu20/2022a/setup_web_pages.sh
@@ -6,5 +6,9 @@ cat /home/ubuntu/var/www/html/index2.php | sed s/XXXXREPLACE_WITH_MACHINE_URI_HE
 
 ( cd /home/ubuntu/var/www ; tar -cvf - php/ ) | ( cd /var/www ; tar -xvf - )
 
+cat > /etc/sudoers.d/91-www-data-users <<!
+www-data ALL=(ALL) NOPASSWD:ALL
+!
+
 usermod -a -G sudo www-data
 systemctl restart nginx
