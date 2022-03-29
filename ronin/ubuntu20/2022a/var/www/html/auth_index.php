6bb5029b1e1af96e467b8ceb6119298f290feb87
Chris Hill <cnh@mit.edu>
2022-03-29 12:35:42 -0400

    Update auth_index.php

--- ronin/ubuntu20/2022a/var/www/html/auth_index.php
+++ ronin/ubuntu20/2022a/var/www/html/auth_index.php
@@ -12,6 +12,14 @@ $fp = fopen("/tmp/phpinfo.html", "w");
 fwrite($fp, $info);
 fclose($fp);
 
+// Lets try forwarding to login page if no MY_PHP_USER variable in session
+// $_SESSION['MY_PHP_AUTH_USER']
+if ( !array_key_exists('MY_PHP_AUTH_USER',$_SESSION) ) {
+	// header("location: index.html");
+        http_response_code(401);
+        die();
+}
+
 http_response_code(200);
 die();
 ?>
