3538bdbb898abb398671d667022c61dd99835a9c
Chris Hill <cnh@mit.edu>
2022-04-05 14:10:16 -0400

    Update setup_vnc_desktop.sh

--- ronin/ubuntu20/2022a/setup_vnc_desktop.sh
+++ ronin/ubuntu20/2022a/setup_vnc_desktop.sh
@@ -9,5 +9,6 @@ chmod u+x /home/ubuntu/.vnc/xstartup
 chown -R ubuntu:ubuntu /home/ubuntu/.vnc/
 # this needs to run as ubuntu
 # vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0
+su -l ubuntu /bin/bash -c "vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0"
 /usr/bin/websockify  -D --verbose    --web /usr/share/novnc/     6080     localhost:5900 --log-file=/var/log/websockify
 apt install -y xterm
