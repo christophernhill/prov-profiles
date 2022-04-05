f20efa763c9565f5ebfb7011492f30a655598f84
Chris Hill <cnh@mit.edu>
2022-04-05 13:57:05 -0400

    Create setup_vnc_desktop.sh

---
+++ ronin/ubuntu20/2022a/setup_vnc_desktop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DEBIAN_FRONTEND=noninteractive apt-get install -y xfce4 tigervnc-standalone-server novnc websockify
+mkdir -p /home/ubuntu/.vnc
+cat > /home/ubuntu/.vnc/xstartup <<'EOFA'
+#!/bin/bash
+startxfce4 &
+EOFA
+chmod u+x /home/ubuntu/.vnc/xstartup
+chown -R ubuntu:ubuntu /home/ubuntu/.vnc/
+# this needs to run as ubuntu
+# vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0
+/usr/bin/websockify  -D --verbose    --web /usr/share/novnc/     6080     localhost:5900 --log-file=/var/log/websockify
