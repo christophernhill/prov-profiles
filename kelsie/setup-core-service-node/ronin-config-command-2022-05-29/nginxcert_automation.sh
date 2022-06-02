89998a7f8dee0622d1ad9a5c46f609538e0ff6ca
KelsieEdie <kelsraines@gmail.com>
2022-06-02 11:15:19 -0400

    Create nginxcert_automation.sh
    Bash script to automate VM Machine set-up with Nginx and Let's Encrypt.

---
+++ kelsie/setup-core-service-node/ronin-config-command-2022-05-29/nginxcert_automation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+curl http://10.0.1.106/add-http-https-access.php > add-http-https-access.sh
+chmod +x add-http-https-access.sh
+./add-http-https-access.sh
+
+#Nginx installation
+#Update local packages and install nginx.
+sudo apt update
+sudo apt install nginx
+echo "System updated and nginx installed."
+
+#Enable ufw.
+sudo ufw enable
+sudo ufw allow 'OpenSSH'
+sudo ufw allow 'Nginx Full'
+sudo ufw status
+echo "Firewall enabled and configured."
+
+#Start nginx web server.
+sudo systemctl reload nginx 
+systemctl status nginx &
+echo "Web server is up and running."
+
+#Let's Encrypt installation
+#Install Certbot.
+sudo apt install certbot python3-certbot-nginx
+sudo systemctl reload nginx
+echo "Cerbot installed and nginx reloaded."
+
+#Get name of the machine in order to get the certificate.
+MACHINE_NAME=`curl http://169.254.169.254/latest/meta-data/tags/instance/dns`
+sudo certbot --nginx --noninteractive --agree-tos --cert-name ${MACHINE_NAME}.mitresearch.cloud -d ${MACHINE_NAME}.mitresearch.cloud --register-unsafely-without-email --nginx --redirect
+sudo systemctl reload nginx
+echo "Certificate issued and nginx reloaded."
+
+#Enable Auto-renewal (???)
