e8194eee352d4332ddb5034e13e148cb1ed7c9b6
KelsieEdie <67616359+KelsieEdie@users.noreply.github.com>
2022-06-06 15:47:07 -0400

    Update nginxcert_automation.sh

--- kelsie/setup-core-service-node/ronin-config-command-2022-05-29/nginxcert_automation.sh
+++ kelsie/setup-core-service-node/ronin-config-command-2022-05-29/nginxcert_automation.sh
@@ -13,30 +13,28 @@ chmod +x add-http-https-access.sh
 #Update local packages.
 sudo apt install nginx
 nginx -v
-echo "System updated and nginx installed."
+echo "Message: System updated and nginx installed."
 
 #Enable ufw.
 sudo ufw enable
 sudo ufw allow 'OpenSSH'
 sudo ufw allow 'Nginx Full'
 sudo ufw status
-echo "Firewall enabled and configured."
+echo "Message: Firewall enabled and configured."
 
 #Start nginx web server.
 sudo systemctl start nginx 
 systemctl status nginx &
-echo "Web server is up and running."
+echo "Message: Web server is up and running."
 
 #Let's Encrypt installation
 #Install Certbot.
 sudo apt install certbot python3-certbot-nginx
 sudo systemctl reload nginx
-echo "Cerbot installed and nginx reloaded."
+echo "Message: Cerbot installed and nginx reloaded."
 
 #Get name of the machine in order to get the certificate.
 MACHINE_NAME=`curl http://169.254.169.254/latest/meta-data/tags/instance/dns`
 sudo certbot --nginx --noninteractive --agree-tos --cert-name ${MACHINE_NAME}.mitresearch.cloud -d ${MACHINE_NAME}.mitresearch.cloud --register-unsafely-without-email --nginx --redirect
 sudo systemctl reload nginx
-echo "Certificate issued and nginx reloaded."
-
-#Enable Auto-renewal (???)
+echo "Message: Certificate issued and nginx reloaded."
