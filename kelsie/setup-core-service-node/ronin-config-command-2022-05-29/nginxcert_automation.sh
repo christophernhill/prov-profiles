cb795b6592f2e8ee9fbce57ac15f3f499be32ecc
KelsieEdie <67616359+KelsieEdie@users.noreply.github.com>
2022-06-08 11:06:42 -0400

    Update nginxcert_automation.sh

--- kelsie/setup-core-service-node/ronin-config-command-2022-05-29/nginxcert_automation.sh
+++ kelsie/setup-core-service-node/ronin-config-command-2022-05-29/nginxcert_automation.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
+#Link to AWS. 
+curl http://10.0.1.106/add-http-https-access.php 
 
 #Update local packages.
 sudo apt update
 
-#Run line of code that Mr.Chris told me to add.
-curl http://10.0.1.106/add-http-https-access.php 
-
-#Download edited nginx default file.
-wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/kelsie/setup-core-service-node/ronin-config-command-2022-05-29/allow_https_traffic.txt
-
 #Nginx installation.
 #Update local packages.
-sudo apt install nginx
-nginx -v
-echo "Message: System updated and nginx installed."
+echo y | sudo apt install nginx
 
 #Enable ufw.
-sudo ufw enable
+echo y | sudo ufw enable 
 sudo ufw allow 'OpenSSH'
 sudo ufw allow 'Nginx Full'
 sudo ufw status
-echo "Message: Firewall enabled and configured."
-
-#Overwrite default file.
-sudo cp allow_https_traffic.txt /etc/nginx/sites-enabled/default
 
 #Start nginx web server.
-sudo systemctl reload nginx
-#sudo systemctl start nginx 
-systemctl status nginx &
-echo "Message: Web server is up and running."
+sudo systemctl start nginx 
+
+#Get and store machine name.
+MACHINE_NAME=`curl http://169.254.169.254/latest/meta-data/tags/instance/dns`
 
 #Let's Encrypt installation
 #Install Certbot.
-sudo apt install certbot python3-certbot-nginx
+echo y | sudo apt install certbot python3-certbot-nginx 
 sudo systemctl reload nginx
-echo "Message: Cerbot installed and nginx reloaded."
 
 #Get name of the machine in order to get the certificate.
-MACHINE_NAME=`curl http://169.254.169.254/latest/meta-data/tags/instance/dns`
+#THIS IS WHERE THE PROBLEM IS 
 sudo certbot --nginx --noninteractive --agree-tos --cert-name ${MACHINE_NAME}.mitresearch.cloud -d ${MACHINE_NAME}.mitresearch.cloud --register-unsafely-without-email --nginx --redirect
 sudo systemctl reload nginx
-echo "Message: Certificate issued and nginx reloaded."
+
+#Test for Website.
+curl https://${MACHINE_NAME}.mitresearch.cloud
