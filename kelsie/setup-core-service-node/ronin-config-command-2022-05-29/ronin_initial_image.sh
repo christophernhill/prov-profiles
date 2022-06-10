fc75d1fc754c83093d6810eff38800362511c8c2
KelsieEdie <67616359+KelsieEdie@users.noreply.github.com>
2022-06-10 18:28:33 -0400

    Update ronin_initial_image.sh

--- kelsie/setup-core-service-node/ronin-config-command-2022-05-29/ronin_initial_image.sh
+++ kelsie/setup-core-service-node/ronin-config-command-2022-05-29/ronin_initial_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# This is the inital image for all the VMs.
 # This will have a single wget that pulls all the wgets to be ran. 
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/kelsie/setup-core-service-node/ronin-config-command-2022-05-29/ronin_startup_config.sh
