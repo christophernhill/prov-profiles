665046fa53829df4c664d34e58fddd13362fab92
KelsieEdie <67616359+KelsieEdie@users.noreply.github.com>
2022-06-10 16:09:56 -0400

    Update ronin_initial_image.sh

--- kelsie/setup-core-service-node/ronin-config-command-2022-05-29/ronin_initial_image.sh
+++ kelsie/setup-core-service-node/ronin-config-command-2022-05-29/ronin_initial_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-# This will have a single wget that pulls the 
+# This will have a single wget that pulls all the wgets to be ran. 
+wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/kelsie/setup-core-service-node/ronin-config-command-2022-05-29/ronin_startup_config.sh
