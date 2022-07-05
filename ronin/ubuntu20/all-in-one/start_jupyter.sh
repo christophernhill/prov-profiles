e47c62c00ea0813d6c8641968c47497925ee2afd
Chris Hill <cnh@mit.edu>
2022-07-05 15:33:45 -0400

    Create start_jupyter.sh

---
+++ ronin/ubuntu20/all-in-one/start_jupyter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+fp=`/home/ubuntu/get_free_port.sh`
+export DISPLAY=:0
+/home/ubuntu/miniconda3/envs/mit-ronin-conda-2022a/bin/jupyter-lab --no-browser --ServerApp.token='' --ServerApp.password='' --port=${fp} --ServerApp.base_url=jupyter/port_${fp} --ServerApp.allow_origin='*'
