a4d3e4e4d9dd31f818c0899fafedc76b8758721e
Chris Hill <cnh@mit.edu>
2022-07-04 21:08:01 -0400

    Update nginxcert_automation.sh

--- ronin/ubuntu20/all-in-one/nginxcert_automation.sh
+++ ronin/ubuntu20/all-in-one/nginxcert_automation.sh
@@ -159,6 +159,10 @@ sudo cat <<'EOFA'
 EOFA
 ) | sudo tee /var/www/html/test_login.php > /dev/null
 
+#Make login page
+\rm fwd2login.php.template
+https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/fwd2login.php.template
+cat fwd2login.php.template | sed s'//'${MACHINE_NAME}'/ | sudo tee /var/www/html/fwd2login.php
 
 
 
