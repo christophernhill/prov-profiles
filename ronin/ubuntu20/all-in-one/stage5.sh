b9d6fc9a387195528254478c6a1950cb9bb61430
Chris Hill <cnh@mit.edu>
2022-07-09 11:17:54 -0400

    Update stage5.sh

--- ronin/ubuntu20/all-in-one/stage5.sh
+++ ronin/ubuntu20/all-in-one/stage5.sh
@@ -17,6 +17,11 @@ cp proxy.lua.template apache2_lua/proxy.lua
 
 \rm apache2.conf.append.template
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/apache2.conf.append.template
+apconf_lua=`grep '/home/ubuntu/apache2_lua' /etc/apache2/apache2.conf | wc -l`
+if [ ${apconf_lua} -eq "0" ]; then
+cat apache2.conf.append.template | sudo tee -a /etc/apache2/apache2.conf
+fi
+systemctl restart apache2
 
 
 echo "End stage 5"
