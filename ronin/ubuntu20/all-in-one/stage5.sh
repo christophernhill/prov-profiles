4e1e0344c2bf03fb3f34aae17ace6e34881050c9
Chris Hill <cnh@mit.edu>
2022-07-09 10:27:13 -0400

    Update stage5.sh

--- ronin/ubuntu20/all-in-one/stage5.sh
+++ ronin/ubuntu20/all-in-one/stage5.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "Begin stage 5"
 
+mkdir apache2_lua
+
+\rm logger.lua.template
+wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/logger.lua.template
+cp logger.lua.template apache2_lua/logger.lua
+
 echo "End stage 5"
