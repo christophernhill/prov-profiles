93cf489b5d12ca481bff99cf5b2b6adf48c09690
Chris Hill <cnh@mit.edu>
2022-07-08 22:12:07 -0400

    Update nginxcert_automation.sh

--- ronin/ubuntu20/all-in-one/nginxcert_automation.sh
+++ ronin/ubuntu20/all-in-one/nginxcert_automation.sh
@@ -8,21 +8,25 @@ date >> automation_stats.txt
 
 echo > install_log.txt
 
+#basic nginx
 \rm stage1.sh
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/stage1.sh
 chmod +x stage1.sh
 ./stage1.sh >> install_log.txt 2>&1
 
+#add lets encrytp nginx
 \rm stage2.sh
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/stage2.sh
 chmod +x stage2.sh
 ./stage2.sh >> install_log.txt 2>&1
 
+#add conda and jupyter lab service
 \rm stage3.sh
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/stage3.sh
 chmod +x stage3.sh
 ./stage3.sh >> install_log.txt 2>&1
 
+#add apache2
 \rm stage4.sh
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/stage4.sh
 chmod +x stage4.sh
