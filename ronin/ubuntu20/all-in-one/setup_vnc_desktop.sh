97e9f885edfa05955c50e6ba793ba872fa91bb2a
Chris Hill <cnh@mit.edu>
2022-07-09 12:43:55 -0400

    Update setup_vnc_desktop.sh

--- ronin/ubuntu20/all-in-one/setup_vnc_desktop.sh
+++ ronin/ubuntu20/all-in-one/setup_vnc_desktop.sh
@@ -6,12 +6,12 @@ cat > /home/ubuntu/.vnc/xstartup <<'EOFA'
 startxfce4 &
 EOFA
 chmod u+x /home/ubuntu/.vnc/xstartup
-chown -R ubuntu:ubuntu /home/ubuntu/.vnc/
+
 # this needs to run as ubuntu
 # vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0
-su -l ubuntu /bin/bash -c "vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0"
-/usr/bin/websockify  -D --verbose    --web /usr/share/novnc/     6080     localhost:5900 --log-file=/var/log/websockify
-apt install -y xterm
+vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0
+sudo /usr/bin/websockify  -D --verbose    --web /usr/share/novnc/     6080     localhost:5900 --log-file=/var/log/websockify
+apt-get install -y xterm
 
 # Fix silly terminal problem
 cat > /home/ubuntu/.config/xfce4/panel/launcher-17/*.desktop <<'EOFA'
@@ -31,4 +31,4 @@ Comment=Use the command line
 X-XFCE-Source=file:///usr/share/applications/exo-terminal-emulator.desktop
 Path=
 EOFA
-chown ubuntu:ubuntu /home/ubuntu/.config/xfce4/panel/launcher-17/*.desktop
+# chown ubuntu:ubuntu /home/ubuntu/.config/xfce4/panel/launcher-17/*.desktop
