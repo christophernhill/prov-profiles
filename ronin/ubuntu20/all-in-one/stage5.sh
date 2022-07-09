3e10e93e31f1bd73ecea375aeca1c9a64aff69c3
Chris Hill <cnh@mit.edu>
2022-07-09 10:28:33 -0400

    Update stage5.sh

--- ronin/ubuntu20/all-in-one/stage5.sh
+++ ronin/ubuntu20/all-in-one/stage5.sh
@@ -7,4 +7,13 @@ mkdir apache2_lua
 wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/logger.lua.template
 cp logger.lua.template apache2_lua/logger.lua
 
+\rm node_proxy.lua.template
+wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/node_proxy.lua.template
+cp node_proxy.lua.template apache2_lua/node_proxy.lua
+
+\rm proxy.lua.template
+wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/proxy.lua.template
+cp proxy.lua.template apache2_lua/proxy.lua
+
+
 echo "End stage 5"
