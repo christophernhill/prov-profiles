12bf66a8bfbfc39e0cc3994164d6358edf22b75e
Chris Hill <cnh@mit.edu>
2022-07-10 17:33:16 -0400

    Update stage7.sh

--- ronin/ubuntu20/all-in-one/stage7.sh
+++ ronin/ubuntu20/all-in-one/stage7.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Begin stage 7"
 
+# Add modules from SC, use NFS for now (later CVMFS ....)
+
 sudo apt-get install -y environment-modules
 sudo apt-get install -y nfs-common
 sudo mkdir -p /state/partition1/llgrid/pkg
