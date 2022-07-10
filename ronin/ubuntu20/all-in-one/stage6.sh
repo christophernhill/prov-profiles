e0bf99d197eb30843555c2bbbefdca9a6e0b9ffc
Chris Hill <cnh@mit.edu>
2022-07-09 23:33:00 -0400

    Update stage6.sh

--- ronin/ubuntu20/all-in-one/stage6.sh
+++ ronin/ubuntu20/all-in-one/stage6.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Begin stage 6"
 
+\rm setup_vnc_desktop.sh
+wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/setup_vnc_desktop.sh
+chmod +x setup_vnc_desktop.sh
+./setup_vnc_desktop.sh
 
 echo "End stage 6"
