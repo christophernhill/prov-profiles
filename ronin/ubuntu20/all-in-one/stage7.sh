1f4e34486311573810d11638737edaabe3aa05fb
Chris Hill <cnh@mit.edu>
2022-07-10 15:54:57 -0400

    Update stage7.sh

--- ronin/ubuntu20/all-in-one/stage7.sh
+++ ronin/ubuntu20/all-in-one/stage7.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "Begin stage 7"
 
-\rm setup_vnc_desktop.sh
-wget https://raw.githubusercontent.com/christophernhill/prov-profiles/main/ronin/ubuntu20/all-in-one/setup_vnc_desktop.sh
-chmod +x setup_vnc_desktop.sh
-./setup_vnc_desktop.sh
+sudo apt-get install -y environment-modules
 
 echo "End stage 7"
