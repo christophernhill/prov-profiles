81c8c978280e4ca8619c19a573371617012de8bd
Chris Hill <cnh@mit.edu>
2022-07-09 22:34:30 -0400

    Update setup_vnc_desktop.sh

--- ronin/ubuntu20/all-in-one/setup_vnc_desktop.sh
+++ ronin/ubuntu20/all-in-one/setup_vnc_desktop.sh
@@ -11,7 +11,7 @@ chmod u+x /home/ubuntu/.vnc/xstartup
 # vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0
 vncserver -localhost yes -geometry 1024x768 -SecurityTypes None  :0
 sudo /usr/bin/websockify  -D --verbose    --web /usr/share/novnc/     6080     localhost:5900 --log-file=/var/log/websockify
-apt-get install -y xterm
+sudo apt-get install -y xterm
 
 # Fix silly terminal problem
 cat > /home/ubuntu/.config/xfce4/panel/launcher-17/*.desktop <<'EOFA'
