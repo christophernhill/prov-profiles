2c1673910839b05cbdacc573a26a226121897785
Chris Hill <cnh@mit.edu>
2023-04-01 23:50:16 -0400

    Update stage1.sh

--- vagrant-local-example/stage1.sh
+++ vagrant-local-example/stage1.sh
@@ -8,9 +8,10 @@ cd ${ROOTDIR}
 vb_and_vg_err=0
 if [ "x$(which VirtualBox)" == "x" ]; then echo Virtual Box not found; vb_and_vg_err=1;  fi
 if [ "x$(which vagrant)" == "x" ]; then echo Vagrant not found; vb_and_vg_err=1;  fi
-if [ "x${vb_and_vg_err}" != "x0"]; then echo ERROR: missing 
+if [ "x${vb_and_vg_err}" != "x0"]; then echo ERROR: missing requirements; exit() ; fi
 
-WORKD_DIR=`mktemp -d -t . _vgrnt `
+WORK_DIR=$(basename `mktemp -u -t vgrnt`)
+mkdir -p ${WORK_DIR}
 cd ${WORK_DIR}
 
 
