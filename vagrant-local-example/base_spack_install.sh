b83fa82af0bcd1829c99c63bbbe4ba6e868d7a48
Chris Hill <cnh@mit.edu>
2023-04-02 17:41:30 -0400

    Update base_spack_install.sh

--- vagrant-local-example/base_spack_install.sh
+++ vagrant-local-example/base_spack_install.sh
@@ -110,8 +110,25 @@ packages:
   gcc:
     compiler: [gcc@8.5.0]
 EOF
-
 vagrant scp spack_externals.yaml :.spack/packages.yaml
+
+cat <<'EOF' > spack_modules.yam
+modules:
+  default:
+    roots:
+      lmod: /home/vagrant/modules/2023-04
+      flat: True
+    arch_folder: false
+    lmod:
+      hash_length: 0
+      exclude_implicits: true
+      core_compilers:
+      - gcc@8.5.0
+      - gcc@12.2.0
+      projections:
+            all: '{name}/{version}'
+EOF
+vagrant scp spack_externals.yaml :.spack/modules.yaml
 
 cat <<EOF | vagrant ssh
 cd spack-2023-04
