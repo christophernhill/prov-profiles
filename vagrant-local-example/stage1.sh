eaee724c24c27c368aa7a1e1d53f9d703f8115d9
Chris Hill <cnh@mit.edu>
2023-04-02 00:17:34 -0400

    Update stage1.sh

--- vagrant-local-example/stage1.sh
+++ vagrant-local-example/stage1.sh
@@ -23,6 +23,7 @@ export VAGRANT_HOME=${MY_VAGRANT_SETUP_ROOT}/.vagrant.d
 export VAGRANT_DOTFILE_PATH=${MY_VAGRANT_SETUP_ROOT}/.vagrant
 
 cd vagrant
+vagrant plugin install vagrant-disksize
 cat >Vagrantfile <<'EOF'
 Vagrant.configure("2") do |config|
   config.vm.box = "generic/rocky8"
