1982c23ebe45cf366b78894be2327dd5ceb22adf
Chris Hill <cnh@mit.edu>
2023-04-01 22:51:23 -0400

    Update stage1.sh

--- vagrant-local-example/stage1.sh
+++ vagrant-local-example/stage1.sh
@@ -5,6 +5,11 @@
 ROOTDIR=.
 cd ${ROOTDIR}
 
+vb_and_vg_err=0
+if [ "x$(which VirtualBox)" == "x" ]; then echo Virtual Box not found; vb_and_vg_err=1;  fi
+if [ "x$(which vagrant)" == "x" ]; then echo Vagrant not found; vb_and_vg_err=1;  fi
+if [ "x${vb_and_vg_err}" != "x0"]; then echo ERROR: missing 
+
 WORKD_DIR=`mktemp -d -t . _vgrnt `
 cd ${WORK_DIR}
 
