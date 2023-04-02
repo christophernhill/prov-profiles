386740c67b0b677a3dfc07744a9534f051abcc5b
Chris Hill <cnh@mit.edu>
2023-04-02 14:10:01 -0400

    Update base_spack_install.sh

--- vagrant-local-example/base_spack_install.sh
+++ vagrant-local-example/base_spack_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #
 #
-# Base provisioning and create an LMOD setup using spack
-# For some reason lmod itself is not well supported by yum, dnf, apt etc....
-# Here we do a base install using a spack with a simple path and against system
-# stack. This can then been used for downstream installs. 
+# Base spack setup
+# 
 #
 
 source ./setup.sh
