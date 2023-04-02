6fea4844b9a6d6c709f61e486238b724f1623384
Chris Hill <cnh@mit.edu>
2023-04-02 12:20:59 -0400

    Update stage2.sh

--- vagrant-local-example/stage2.sh
+++ vagrant-local-example/stage2.sh
@@ -17,5 +17,6 @@ sudo dnf config-manager --set-enabled powertools
 sudo yum -y install git
 sudo yum -y install lua lua-filesystem lua-posix tcl
 sudo yum -y install unzip
+sudo yum -y install Lmod
 
 EOF
