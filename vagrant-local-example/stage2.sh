1f70d559ca1f4642c7e9e10c9c400a8efa9476eb
Chris Hill <cnh@mit.edu>
2023-04-02 12:12:12 -0400

    Update stage2.sh

--- vagrant-local-example/stage2.sh
+++ vagrant-local-example/stage2.sh
@@ -10,4 +10,6 @@ sudo dnf -y install dnf-plugins-core
 sudo dnf upgrade -y
 sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf config-manager --set-enabled powertools
+
+sudo yum -y install git
 EOF
