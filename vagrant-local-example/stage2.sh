56f65aeab70758b78ca3ff201e3c99f7e17c4971
Chris Hill <cnh@mit.edu>
2023-04-02 21:31:23 -0400

    Update stage2.sh

--- vagrant-local-example/stage2.sh
+++ vagrant-local-example/stage2.sh
@@ -24,6 +24,7 @@ sudo yum -y install bison bison-devel bison-runtime
 sudo yum -y install gmp gmp-c++ gmp-devel
 sudo yum -y install libmpc libmpc-devel
 sudo yum -y install numactl
+sudo yum -y install rdma-core rdma-core-devel ucx-rdmacm librdmacm
 
 
 EOF
